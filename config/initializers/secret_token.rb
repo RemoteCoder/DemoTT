8072b57e0057e948841e20e16ced8178d1d5d5a3
RemoteCoder <shah.duet@gmail.com>
2013-05-02 00:43:39 +0600

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoTt::Application.config.secret_token = '2f2902654e4042a80c8dff0a998f7450986ca43dfa17c563ddf258c6191b827ddf9d89a00e384ac152bc3205ba4eda526505cf96e7ec019745c7fa5481c90f12'
