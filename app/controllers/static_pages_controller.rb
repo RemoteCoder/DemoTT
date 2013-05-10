079d2c443cb1b6f6ec89dd9eb3ad5ffece12ce10
RemoteCoder <shah.duet@gmail.com>
2013-05-11 02:18:28 +0600

    heroku dpeloy

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @micropost = current_user.microposts.build if signed_in?
-    @feed_items = current_user.feed.page(params[:page])
+    @feed_items = current_user.feed.page(params[:page]) rescue nil
   end
 
   def help
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ DemoTt::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
-  match '/signup',  to: 'users#new'
+  get '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
   root :to => "static_pages#home"
