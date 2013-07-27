e825e76e9655542da1d6db46e1435e9898d85b70
RemoteCoder <shah.duet@gmail.com>
2013-07-27 11:33:30 +0600

    log the auth value

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,8 +36,8 @@ class User < ActiveRecord::Base
   #validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
-  validates :password             , length: { minimum: 6 }, :if => :validate_password?
-  validates :password_confirmation, presence: true        , :if => :validate_password?
+  #validates :password             , length: { minimum: 6 }, :if => :validate_password?
+  #validates :password_confirmation, presence: true        , :if => :validate_password?
 
 
   #alias :devise_valid_password? :valid_password?
